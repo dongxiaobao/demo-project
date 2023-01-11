7c0d647e6cc4c962f3e6d9da7b57147e25707786
admin <18363992775@163.com>
2023-01-11 19:51:18 +0800

    修改upcoming events

--- src/views/Home/overview.vue
+++ src/views/Home/overview.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="over-view">
     <header class="header">
-      <el-carousel indicator-position="outside" autoplay :pause-on-hover='false'>
+      <el-carousel
+        indicator-position="outside"
+        autoplay
+        :pause-on-hover="false"
+      >
         <el-carousel-item v-for="(item, index) in imgList" :key="index">
           <img :src="item.imgSrc" alt="" class="pic-class" />
         </el-carousel-item>
@@ -20,21 +24,34 @@
         </p>
         <div class="work-shop">
           <div class="left-date">
-            <span class="number-data">18</span>
-            <span class="month-data">Jan</span>
+            <span class="number-data">6</span>
+            <span class="month-data">Feb</span>
           </div>
           <div class="right">
             <p class="title-top">
-              (De)coding AI: Examining the political and environmental impact of
-              the lifecycle of algorithms
+              Celebrating the 50th Anniversary of Agenda-setting Theory Where
+              Have We Been and Where Are We Heading
             </p>
-            <p class="desc-1">10:00 -18:00, Hybrid</p>
-            <p class="desc-2">With Dr Ana Valdivia</p>
-            <p class="desc-3">
-              This workshop will explore the interactions and controversies of
-              algorithms beyond the code.
+            <p class="desc-1">
+              <span class="label"> Speaker(s): </span>
+              <span class="value">
+                Prof. Maxwell E. McCombs, Prof. David H. Weaver, Prof. Jonathan
+                J. H. Zhu, Prof. James W. Dearing and Prof. Tai-Quan “Winson”
+                Peng
+              </span>
             </p>
-            <p class="desc-4">Learn more & Book</p>
+            <p class="desc-2">
+              <span class="label">Time: </span>
+              <span class="value"
+                >6 February 2023 (Mon) 9:00 am - 11:00 am (HKT)</span
+              >
+            </p>
+            <p class="desc-2">
+              <span class="label">Venue: </span>
+              <span class="value">Zoom</span>
+            </p>
+
+            <p class="desc-4" @click="learnAndBook">Learn more & Book</p>
           </div>
         </div>
         <!-- <p class="title-2">Past Webinars</p>
@@ -68,6 +85,11 @@ let imgList = ref([
     imgSrc: pic03,
   },
 ]);
+const learnAndBook = () => {
+  window.open(
+    "http://www.cityu.edu.hk/com/Page.aspx?p=ResearchSeminars&id=242"
+  );
+};
 </script>
 
 <style lang="scss">
@@ -127,9 +149,9 @@ let imgList = ref([
         }
       }
       .work-shop {
-        height: 300px;
+        height: 320px;
         position: relative;
-       background: rgb(167,219,251);
+        background: rgb(167, 219, 251);
         overflow: hidden;
         .left-date {
           width: 180px;
@@ -142,7 +164,7 @@ let imgList = ref([
           .number-data {
             position: absolute;
             top: 40px;
-            right: 40px;
+            right: 60px;
             font-size: 60px;
             line-height: 1.5;
             color: #fff;
@@ -154,49 +176,51 @@ let imgList = ref([
             line-height: 1.5;
             color: #fff;
             font-weight: 700;
-            top: 105px;
+            top: 110px;
             right: 50px;
           }
         }
         .right {
           margin-left: 200px;
           margin-top: 20px;
-          margin-right: 40px;
+          margin-right: 60px;
           .title-top {
             font-size: 20px;
             line-height: 1.5;
             color: #222;
+            font-weight: 600;
           }
           .desc-1 {
             font-size: 16px;
             line-height: 1.5;
             color: #222;
             margin: 20px 0;
-            font-style: italic;
+            .label {
+              font-weight: 600;
+              margin-right: 10px;
+            }
           }
           .desc-2 {
             font-size: 16px;
             line-height: 1.5;
             color: #222;
             margin: 20px 0;
+            .label {
+              font-weight: 600;
+              margin-right: 10px;
+            }
           }
-          .desc-3 {
-            font-size: 16px;
-            line-height: 1.5;
-            color: #222;
-            margin: 20px 0;
-          }
+
           .desc-4 {
             display: inline-block;
             font-size: 14px;
             line-height: 1.5;
             padding: 8px 30px;
             border-radius: 20px;
-             color: #222;
+            color: #222;
             font-weight: 600;
-            background: rgb(67,160,234);
+            background: rgb(67, 160, 234);
             cursor: pointer;
-            margin: 20px 0;
           }
         }
       }
